e7bb82643cb2c3b51d90d24284bbaa6ab12dae5b
IpMan522 <mihailanatolevic844@gmail.com>
2021-02-03 12:44:47 +0700

    добавил к тексту «Всё, что нам нужно – это» сгенерированную фразу и вывел её

--- app/src/main/java/com/example/mystring/MainActivity.java
+++ app/src/main/java/com/example/mystring/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.mystring;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+
+        String s = "Всё, что нам нужно – это " + Phraser.PhraserGen();
+        text.setText(s);
+
     }
 }
